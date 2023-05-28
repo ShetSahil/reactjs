60ff4d1194950f68ec30423472ed1447c1f09e59
ShetSahil <20104122.sahilshetty@gmail.com>
2023-05-28 13:13:04 +0530

    no change

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForms from './Components/TextForms';
+import About from './Components/About';
+import { useState } from 'react';
+
 
 
 function App() {
+  const[mode,setMode]=useState("light")
+  // const togglemode=() =>{
+  //   if(mode==='light'){
+  //     setMode('dark')
+  //   }else{
+  //     setMode('light')
+  //   }
+  // }
+  
   return (
     <>
-    <Navbar title="TextUtils" about="About Textutils"></Navbar>
+    <Navbar title="TextUtils" about="About Textutils" mode={mode}></Navbar>
     {/* <Navbar></Navbar> */}
     <TextForms heading="Enter The Text To Analyze"/>
+    {/* <About/> */}
     </>
   );
 }
--- src/Components/TextForms.js
+++ src/Components/TextForms.js
@@ -5,6 +5,15 @@ export default function TextForms(props) {
         let mytext = text.toUpperCase();
         setText(mytext);
     }
+    const downHandle = ()=>{
+      let mytext = text.toLowerCase();
+      setText(mytext);
+  }
+  const capHandle = ()=>{
+    setText('');
+    
+    
+}
     const handleChange = (event)=>{
 
         setText(event.target.value)
@@ -20,8 +29,13 @@ export default function TextForms(props) {
     <div className="container my-3">
     <h1>{props.heading}</h1>
     <textarea className="form-control my-3" id="box" rows="10" value={text} onChange={handleChange}></textarea>
-    <button type="button" className="btn btn-primary" onClick={upHandle}>UpperCase</button>
+    <button type="button " className="btn btn-primary " onClick={upHandle}>UpperCase</button>
+    <button type="button" className="btn btn-primary mx-3" onClick={downHandle}>LowerCase</button>
+    <button type="button" className="btn btn-primary mx-3" onClick={capHandle}>Clear Text</button>
 
+    <h1>Preview:</h1>
+    <p>{text}</p>
+    <h1>length:{text.split(" ").length} words  {text.length} characters</h1>
     </div>
   </div>
 
